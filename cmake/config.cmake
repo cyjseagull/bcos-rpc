ccbdf9ba20b7473586d5905dff18a04ecb5d97cf
octopus <912554887@qq.com>
2021-06-17 16:31:50 +0800

    add CMakeLists.txt

---
+++ cmake/config.cmake
@@ -0,0 +1,5 @@
+hunter_config(bcos-framework
+	VERSION 3.0.0-local
+	URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/6953162bd31da403161d46d0f4a7b887d249e452.tar.gz"
+	SHA1 59114238677eabbf12b584041ff933f231dd13c6
+)
