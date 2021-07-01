2a628cd9240bbbc46e50756a6796070d9b807f92
octopus <912554887@qq.com>
2021-07-01 15:34:39 +0800

    modify bcos-framework url

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,5 @@
 hunter_config(bcos-framework
 	VERSION 3.0.0-local
-	URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/6953162bd31da403161d46d0f4a7b887d249e452.tar.gz"
-	SHA1 59114238677eabbf12b584041ff933f231dd13c6
+	URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/917ce1948babc30d7eb7c72aa854637df5eed818.tar.gz"
+	SHA1 b2d83a768978cbc9cc65883e503fafcc6660e10a
 )
