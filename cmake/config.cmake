e2102b0544d1d8a3bf52bb7b0f173199698b92d8
octopus <912554887@qq.com>
2021-07-01 16:02:25 +0800

    update bcos-crypto url for compile

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,3 +1,8 @@
+hunter_config(bcos-crypto
+	VERSION 3.0.0-local
+	URL "https://${URL_BASE}/FISCO-BCOS/bcos-crypto/archive/25c8edb7d5cbadb514bbce9733573c8ffdb3600d.tar.gz"
+	SHA1 4a1649e7095f5db58a5ae0671b2278bcccc25f1d
+)
 hunter_config(bcos-framework
 	VERSION 3.0.0-local
 	URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/917ce1948babc30d7eb7c72aa854637df5eed818.tar.gz"
