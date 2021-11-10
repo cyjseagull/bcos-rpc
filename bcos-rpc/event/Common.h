e37096fdcfa6298f46062b9b1082a4e3eae967f2
XingQiang Bai <bxq2011hust@qq.com>
2021-11-10 20:17:46 +0800

    remove wrong licence (#72)

--- bcos-rpc/event/Common.h
+++ bcos-rpc/event/Common.h
@@ -1,19 +1,4 @@
 /**
- * @CopyRight:
- * FISCO-BCOS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * FISCO-BCOS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with FISCO-BCOS.  If not, see <http:www.gnu.org/licenses/>
- * (c) 2019-2021 fisco-dev contributors.
- *
  * @file Common.h
  * @author: octopuswang
  * @date 2019-08-13
